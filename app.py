8feb0797895f7aefc82b0d3901ad0dd449f230b0
Yash Khandelwal <yashsk23.12@gmail.com>
2024-07-12 01:01:43 +0530

    adding update page and functionality

--- app.py
+++ app.py
@@ -44,7 +44,8 @@ def addBook():
 
 @app.route("/records")
 def records():
-    return render_template("records.html")
+    allBooks=Book.query.all()
+    return render_template("records.html", allBooks=allBooks)
 
 
 @app.route("/add", methods=['GET','POST'])
@@ -63,7 +64,41 @@ def add():
         return redirect("/")
 
 
+@app.route('/delete')
+def delete():
 
+    #extracting the booknumber
+    BookNo=request.args.get('BookNo')
+
+    book=Book.query.filter_by(BookNo=BookNo).first()
+
+    #deleting book
+    database.session.delete(book)
+    database.session.commit()
+
+
+    #redirecting to the main page
+    return  redirect('/records')
+
+@app.route("/update", methods=["GET", "POST"])
+def update():
+    #getting for which book we have request
+    bookNo=request.args.get("BookNo")
+
+    if request.method=="POST":
+        bookNo=request.form.get("BookNo")
+        bookName=request.form.get("BookName")
+        bookAuthor=request.form.get("BookAuthor")
+        bookGenre=request.form.get("BookGenre")
+
+        bookRecord = Book(BookNo=bookNo,BookName=bookName,BookAuthor=bookAuthor,BookGenre=bookGenre)
+        print(bookNo)
+        database.session.add(bookRecord)
+        database.session.commit()
+        return redirect("/records")
+
+    currentBook=Book.query.filter_by(BookNo=bookNo).first()
+    return render_template("update.html", currentBook=currentBook)
 
 
 
