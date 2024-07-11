13e823215a936d2dca143f918bfa2fdbc3d8c703
Yash Khandelwal <yashsk23.12@gmail.com>
2024-07-11 23:31:36 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+
+#creating app
+app=Flask(__name__)
+
+#connecting our app to sqlite database
+#this command will tell ourr program to connect with a sqlite database named taskmanager.db 
+app.config["SQLALCHEMY_DATABASE_URI"]='sqlite:///records.db'
+
+#creating an object of sqlalchemy, parameter tells which flask app to connect to
+database= SQLAlchemy(app) 
+
+class Book(database.Model):
+    BookNo=database.Column(database.String(10), primary_key=True)
+
+    BookName=database.Column(database.String(100), nullable=False)
+
+    BookAuthor= database.Column(database.String(100), nullable=False)
+
+    BookGenre=database.Column(database.String(100), nullable=False)
+
+
+
+
+@app.route('/')
+def index():
+    return render_template("index.html",)
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/add", methods=['GET','POST'])
+def add():
+    if request.method=="POST":
+        bookNo=request.form.get("BookNo")
+        bookName=request.form.get("BookName")
+        bookAuthor=request.form.get("BookAuthor")
+        bookGenre=request.form.get("BookGenre")
+
+        bookRecord = Book(BookNo=bookNo,BookName=bookName,BookAuthor=bookAuthor,BookGenre=bookGenre)
+
+        database.session.add(bookRecord)
+        database.session.commit()
+
+        return redirect("/")
+
+
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True) #running app
